7fecb292028fae2871a986b3bc12289e50192df7
Antoinette Smith <antoinette@primeacademy.io>
2017-01-05 16:23:21 -0600

    updated

--- group_01.js
+++ group_01.js
@@ -1,8 +1,38 @@
-var atticus = ["Atticus", "2405", "47000", 3];
-var jem = ["Jem", "62347", "63500", 4];
-var boo = ["Boo", "11435", "54000", 3];
-var scout = ["Scout", "6243", "74750", 5];
-var robert = ["Robert", "26835", "66000", 1];
-var mayella = ["Mayella", "89068", "35000", 2];
+var atticus = {
+  employeeName: 'Atticus',
+  employeeNumber: '2405',
+  annualSalary: '47000',
+  reviewRating: 3,
+};
+var jem = {
+  employeeName: 'Jem',
+  employeeNumber: '62347',
+  annualSalary: '63500',
+  reviewRating: 4,
+};
+var boo = {
+  employeeName: 'Boo',
+  employeeNumber: '11435',
+  annualSalary: '54000',
+  reviewRating: 3,
+};
+var scout = {
+  employeeName: 'Scout',
+  employeeNumber: '6243',
+  annualSalary: '74750',
+  reviewRating: 5,
+};
+var robert = {
+  employeeName: 'Robert',
+  employeeNumber: '26835',
+  annualSalary: '66000',
+  reviewRating: 1,
+};
+var mayella = {
+  employeeName: 'Mayella',
+  employeeNumber: '89068',
+  annualSalary: '35000',
+  reviewRating: 2,
+};
 
 var employees = [atticus, jem, boo, scout, robert, mayella];
